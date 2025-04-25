53440a264f3bbb2c2460a66605323902b35492a3
anshikagupta170903 <112505363+anshikagupta170903@users.noreply.github.com>
2025-04-26 01:59:15 +0530

    Your update message

--- src/components/Cart/CartTotal.js
+++ src/components/Cart/CartTotal.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
-export default function CartTotal({value}) {
+import PayPalButton from './PayPalButton';
+export default function CartTotal({value,history}) {
     const{cartSubTotal,cartTax,cartTotal,clearCart}=value; 
   return (
     <React.Fragment>
@@ -32,6 +33,9 @@ export default function CartTotal({value}) {
                 </span>
                 <strong>${cartTotal}</strong>
               </h5>
+              <PayPalButton total={cartTotal} clearCart={clearCart} history={history} />
+
+              
             </div>
           </div>
         </div>
